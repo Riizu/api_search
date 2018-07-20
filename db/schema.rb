c03f695f232a0e8242fe13b9ac7da0c26323ca15
Ryan Batty <rgbatty@outlook.com>
2018-07-20 16:37:13 -0600

    Add reference column to actions table for monster

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_223458) do
+ActiveRecord::Schema.define(version: 2018_07_20_223545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2018_07_20_223458) do
     t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "monster_id"
+    t.index ["monster_id"], name: "index_actions_on_monster_id"
   end
 
   create_table "monsters", force: :cascade do |t|
@@ -84,4 +86,5 @@ ActiveRecord::Schema.define(version: 2018_07_20_223458) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "actions", "monsters"
 end
