d9d4d39b35e1ad161fa8896afee7ffc887123513
Ryan Batty <rgbatty@outlook.com>
2018-07-22 21:41:36 -0600

    Adjust keyword search to ignore case

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,6 @@
 class Search < ApplicationRecord
+    before_create :check_params
+
     validates :keyword, uniqueness: true, presence: true
     validates :num_times, presence: true
 
@@ -9,14 +11,18 @@ class Search < ApplicationRecord
 
     def monster_search
         monster_queries = [
-            "name LIKE ?", "size LIKE ?", "monster_type LIKE ?", "subtype LIKE ?",
-            "alignment LIKE ?", "speed LIKE ?", "damage_vulnerabilities LIKE ?",
-            "damage_resistances LIKE ?", "damage_immunities LIKE ?", 
-            "condition_immunities LIKE ?", "senses LIKE ?", "languages LIKE ?"]
+            "lower(name) LIKE ?", "lower(size) LIKE ?", "lower(monster_type) LIKE ?", "lower(subtype) LIKE ?",
+            "lower(alignment) LIKE ?", "lower(speed) LIKE ?", "lower(damage_vulnerabilities) LIKE ?",
+            "lower(damage_resistances) LIKE ?", "lower(damage_immunities) LIKE ?", 
+            "lower(condition_immunities) LIKE ?", "lower(senses) LIKE ?", "lower(languages) LIKE ?"]
         query_string = monster_queries.join(" OR ")
 
-        Monster.where(query_string, "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", 
-            "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", 
-            "%#{keyword}%", "%#{keyword}%", "%#{keyword}%")
+        Monster.where(query_string, "%#{keyword.downcase}%", "%#{keyword.downcase}%", "%#{keyword.downcase}%", 
+            "%#{keyword.downcase}%", "%#{keyword.downcase}%", "%#{keyword.downcase}%", "%#{keyword.downcase}%", "%#{keyword.downcase}%", "%#{keyword.downcase}%", 
+            "%#{keyword.downcase}%", "%#{keyword.downcase}%", "%#{keyword.downcase}%")
+    end
+
+    def check_params
+        self.keyword.downcase!
     end
 end
